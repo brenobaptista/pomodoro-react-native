99972374e73d00ac4b38d3b89f8d461ffa6145c5
brenobaptista <brenomb07@hotmail.com>
2020-01-19 14:30:16 -0300

    Timer Unit Testing

--- __tests__/Checkmark.test.js
+++ __tests__/Checkmark.test.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import Checkmark from '../src/components/Checkmark/Checkmark';
 import toJson from 'enzyme-to-json';
+import Checkmark from '../src/components/Checkmark/Checkmark';
 
-describe('Checkmark', () => {
-    describe('Rendering', () => {
-        it('should match to snapshot - True', () => {
-            const component = shallow(<Checkmark check={true} />)
-            expect(toJson(component)).toMatchSnapshot("True checkmark snapshot")
-        });
-        it('should match to snapshot - False', () => {
-            const component = shallow(<Checkmark check={false}/>)
-            expect(toJson(component)).toMatchSnapshot("False checkmark snapshot")
-        });
-    });
+describe('Testing Checkmark Component', () => {
+  it('should match to snapshot - True', () => {
+    const wrapper = shallow(<Checkmark check={true} />);
+
+    expect(toJson(wrapper)).toMatchSnapshot('True checkmark snapshot');
+  });
+
+  it('should match to snapshot - False', () => {
+    const wrapper = shallow(<Checkmark check={false} />);
+    
+    expect(toJson(wrapper)).toMatchSnapshot('False checkmark snapshot');
+  });
 });
---
+++ __tests__/Timer.test.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { shallow } from "enzyme";
+import toJson from 'enzyme-to-json';
+import { Provider } from "react-redux";
+import configureStore from 'redux-mock-store';
+import Timer from '../src/components/Timer/Timer';
+
+const mockStore = configureStore();
+
+const initialState = {
+  timer: 1500,
+  initialTimer: 1500,
+};
+
+const store = mockStore(initialState);
+
+describe('Testing Timer Component', () => {
+  it('should match to snapshot', () => {
+    const wrapper = shallow(
+      <Provider store={store}>
+        <Timer />, 
+      </Provider>
+    );
+
+    expect(toJson(wrapper)).toMatchSnapshot('Timer snapshot');
+  });
+});
