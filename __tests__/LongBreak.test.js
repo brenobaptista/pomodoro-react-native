b8a2cbbb4dbec09952876f4caa28545c31ff580c
brenobaptista <brenomb07@hotmail.com>
2020-01-19 14:44:31 -0300

    Short and Long Break Unit Testing

---
+++ __tests__/LongBreak.test.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { shallow } from "enzyme";
+import toJson from 'enzyme-to-json';
+import { Provider } from "react-redux";
+import configureStore from 'redux-mock-store';
+import LongBreak from '../src/components/LongBreak/LongBreak';
+
+const mockStore = configureStore();
+
+const initialState = {
+  timer: 1800,
+  initialTimer: 1800,
+};
+
+const store = mockStore(initialState);
+
+describe('Testing Long Break Component', () => {
+  it('should match to snapshot', () => {
+    const wrapper = shallow(
+      <Provider store={store}>
+        <LongBreak />, 
+      </Provider>
+    );
+
+    expect(toJson(wrapper)).toMatchSnapshot('Long Break snapshot');
+  });
+});
---
+++ __tests__/ShortBreak.test.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { shallow } from "enzyme";
+import toJson from 'enzyme-to-json';
+import { Provider } from "react-redux";
+import configureStore from 'redux-mock-store';
+import ShortBreak from '../src/components/ShortBreak/ShortBreak';
+
+const mockStore = configureStore();
+
+const initialState = {
+  timer: 300,
+  initialTimer: 300,
+};
+
+const store = mockStore(initialState);
+
+describe('Testing Short Break Component', () => {
+  it('should match to snapshot', () => {
+    const wrapper = shallow(
+      <Provider store={store}>
+        <ShortBreak />, 
+      </Provider>
+    );
+
+    expect(toJson(wrapper)).toMatchSnapshot('Short Break snapshot');
+  });
+});
